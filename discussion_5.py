4f0958641f5b46cb15c0a74f3d747ec1d659348b
ijsmithh <ijsmithh@umich.edu>
2022-09-30 21:06:01 -0400

    commit 4

--- discussion_5.py
+++ discussion_5.py
@@ -50,7 +50,11 @@ class Warehouse:
 	
 	# Returns the item in the warehouse with the highest price
 	def get_max_price(self):
-		pass	
+		max_price = self.items[0]
+		for item in self.items:
+			if item.price > max_price.price:
+				max_price = item
+		return max_price.name
 
 
 
